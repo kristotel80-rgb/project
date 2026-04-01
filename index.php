c68fdfdd302b526b0bb5fb7f632a754b86dab7fd
gratia <kristotel80@gmail.com>
2026-04-01 15:37:04 +0700

    update

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 require_once 'transferbank.php';
 require_once 'ewallet.php';
 require_once 'qris.php';
+require_once 'cod.php';
 
 // objek
 $transfer = new TransferBank(100000);
 $ewallet  = new EWallet(50000);
 $qris     = new QRIS(75000);
+$cod     = new COD(80000);
 
 // output
 echo $transfer->prosesPembayaran();
@@ -24,4 +26,10 @@ echo "<hr>";
 echo $qris->prosesPembayaran();
 echo "<br>";
 echo $qris->cetakStruk();
+
+echo "<hr>";
+
+echo $cod->prosesPembayaran();
+echo "<br>";
+echo $cod->cetakStruk();
 ?>
